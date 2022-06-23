475895d5de24530030276d17be03e4b6c7e6f419
Wilton Souza <willsouzafilho@gmail.com>
2022-06-23 14:09:42 -0300

    refactor: Refactoring the exceptionHandler

--- src/main/java/br/com/itau/challenge/exceptions/handler/ApiExceptionHandler.java
+++ src/main/java/br/com/itau/challenge/exceptions/handler/ApiExceptionHandler.java
@@ -1,139 +1,93 @@
 package br.com.itau.challenge.exceptions.handler;
 
+import br.com.itau.challenge.dtos.response.ErrorResponseDTO;
 import br.com.itau.challenge.exceptions.*;
 import lombok.AllArgsConstructor;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.time.OffsetDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import javax.validation.ConstraintViolationException;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @RestControllerAdvice
-public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+public class ApiExceptionHandler {
 
     private final MessageSource messageSource;
 
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        List<Error.Field> fields = new ArrayList<>();
-
-        for (ObjectError error : ex.getBindingResult().getAllErrors()) {
-            String name = ((FieldError) error).getField();
-            String message = messageSource.getMessage(error, LocaleContextHolder.getLocale());
-
-            fields.add(new Error.Field(name, message));
-        }
-
-        Error error = new Error();
-        error.setStatus(status.value());
-        error.setTime(OffsetDateTime.now());
-        error.setMessage("Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente");
-        error.setFields(fields);
-
-        return handleExceptionInternal(ex, error, headers, status, request);
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(code = HttpStatus.UNPROCESSABLE_ENTITY)
+    public Collection<ErrorResponseDTO> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException exception) {
+        return exception.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(violation -> ErrorResponseDTO.as(
+                                messageSource.getMessage(violation, LocaleContextHolder.getLocale()))
+                        .tag(simpleKey(violation))).collect(Collectors.toList());
     }
 
-    @ExceptionHandler(UserAlreadyExistsException.class)
-    @ResponseStatus(HttpStatus.CONFLICT)
-    public ResponseEntity<Object> handleUserAlreadyExists(RuntimeException ex, WebRequest request) {
-        HttpStatus status = HttpStatus.CONFLICT;
-
-        Error error = new Error();
-        error.setStatus(status.value());
-        error.setTime(OffsetDateTime.now());
-        error.setMessage(ex.getMessage());
-
-        return handleExceptionInternal(ex, error, new HttpHeaders(), status, request);
-    }
-
-    @ExceptionHandler(UserNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ResponseEntity<Object> handleUserNotFound(RuntimeException ex, WebRequest request) {
-        HttpStatus status = HttpStatus.NOT_FOUND;
-
-        Error error = new Error();
-        error.setStatus(status.value());
-        error.setTime(OffsetDateTime.now());
-        error.setMessage(ex.getMessage());
-
-        return handleExceptionInternal(ex, error, new HttpHeaders(), status, request);
-    }
-
-    @ExceptionHandler(UserAlreadyHaveCardException.class)
-    @ResponseStatus(HttpStatus.CONFLICT)
-    public ResponseEntity<Object> handleUserAlreadyHaveCard(UserAlreadyHaveCardException ex, WebRequest request) {
-        HttpStatus status = HttpStatus.CONFLICT;
-
-        Error error = new Error();
-        error.setStatus(status.value());
-        error.setTime(OffsetDateTime.now());
-        error.setMessage(ex.getMessage());
-
-        return handleExceptionInternal(ex, error, new HttpHeaders(), status, request);
-    }
-
-    @ExceptionHandler(UserDoesNotHaveCardException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ResponseEntity<Object> handleDoesNotHaveCardException(UserDoesNotHaveCardException ex, WebRequest request) {
-        HttpStatus status = HttpStatus.NOT_FOUND;
-
-        Error error = new Error();
-        error.setStatus(status.value());
-        error.setTime(OffsetDateTime.now());
-        error.setMessage(ex.getMessage());
-
-        return handleExceptionInternal(ex, error, new HttpHeaders(), status, request);
-    }
-
-    @ExceptionHandler(PurchaseNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ResponseEntity<Object> handlePurchaseNotFound(PurchaseNotFoundException ex, WebRequest request) {
-        HttpStatus status = HttpStatus.NOT_FOUND;
-
-        Error error = new Error();
-        error.setStatus(status.value());
-        error.setTime(OffsetDateTime.now());
-        error.setMessage(ex.getMessage());
-
-        return handleExceptionInternal(ex, error, new HttpHeaders(), status, request);
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    public Collection<ErrorResponseDTO> handleConstraintViolationException(
+            ConstraintViolationException exception) {
+        return exception.getConstraintViolations()
+                .stream()
+                .map(violation -> ErrorResponseDTO.as(violation.getMessage())
+                        .tag(violation.getPropertyPath().toString())).collect(Collectors.toList());
     }
 
     @ExceptionHandler(ContestationNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ResponseEntity<Object> handleContestationNotFound(ContestationNotFoundException ex, WebRequest request) {
-        HttpStatus status = HttpStatus.NOT_FOUND;
-
-        Error error = new Error();
-        error.setStatus(status.value());
-        error.setTime(OffsetDateTime.now());
-        error.setMessage(ex.getMessage());
-
-        return handleExceptionInternal(ex, error, new HttpHeaders(), status, request);
+    @ResponseStatus(code = HttpStatus.NOT_FOUND)
+    public ErrorResponseDTO handleContestationNotFound(ContestationNotFoundException exception) {
+        return ErrorResponseDTO.as(message("ContestationNotFoundException.message"));
     }
 
-    @ExceptionHandler(ForbiddenException.class)
-    @ResponseStatus(HttpStatus.FORBIDDEN)
-    public ResponseEntity<Object> handleForbidden(ForbiddenException ex, WebRequest request) {
-        HttpStatus status = HttpStatus.FORBIDDEN;
-
-        Error error = new Error();
-        error.setStatus(status.value());
-        error.setTime(OffsetDateTime.now());
-        error.setMessage(ex.getMessage());
-
-        return handleExceptionInternal(ex, error, new HttpHeaders(), status, request);
+    @ExceptionHandler(PurchaseNotFoundException.class)
+    @ResponseStatus(code = HttpStatus.NOT_FOUND)
+    public ErrorResponseDTO handlePurchaseNotFound(PurchaseNotFoundException exception) {
+        return ErrorResponseDTO.as(message("PurchaseNotFoundException.message"));
     }
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    @ResponseStatus(code = HttpStatus.CONFLICT)
+    public ErrorResponseDTO handleUserAlreadyExists(UserAlreadyExistsException exception) {
+        return ErrorResponseDTO.as(message("UserAlreadyExistsException.message"));
+    }
+
+    @ExceptionHandler(UserDoesNotHaveCardException.class)
+    @ResponseStatus(code = HttpStatus.CONFLICT)
+    public ErrorResponseDTO handleUserDoesNotHaveCard(UserDoesNotHaveCardException exception) {
+        return ErrorResponseDTO.as(message("UserDoesNotHaveCardException.message"));
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    @ResponseStatus(code = HttpStatus.CONFLICT)
+    public ErrorResponseDTO handleUserNotFound(UserNotFoundException exception) {
+        return ErrorResponseDTO.as(message("UserNotFoundException.message"));
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponseDTO handleInternalServerError(Exception exception) {
+        return ErrorResponseDTO.as(message("UserNotFoundException.message"));
+    }
+
+    private String simpleKey(ObjectError violation) {
+        return violation instanceof FieldError ? ((FieldError) violation).getField()
+                : violation.getObjectName();
+    }
+
+    private String message(String code, Object... params) {
+        return messageSource.getMessage(code, params, LocaleContextHolder.getLocale());
+    }
+
 }
